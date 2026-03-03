fc7da6a956b53289da072463f760efb9babc91ff
Alexander Gulin <gonimani1@gmail.com>
2026-03-04 00:13:21 +0300

    Change standart options of SnippetForm

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,15 +1,29 @@
-from django.forms import ModelForm,  ValidationError
+from django.forms import ModelForm, TextInput, Textarea,  ValidationError
 from MainApp.models import Snippet
 
 
 class SnippetForm(ModelForm):
-   class Meta:
-       model = Snippet
-       # Описываем поля, которые будем заполнять в форме
-       fields = ['name', 'lang', 'code']
+   
+    class Meta:
+            model = Snippet
+            # Описываем поля, которые будем заполнять в форме
+            fields = ['name', 'lang', 'code']
+            labels = {"name": "", "lang": "", "code": ""}
+            widgets = {
+                "name": TextInput(attrs={
+                    "class": "form-control",
+                    "placeholder": "Название сниппета",
+                    "style": "max-width: 300px"
+                }),
+                "code": Textarea(attrs={
+                    "placeholder": "Код сниппета",
+                    "rows": 5,
+                    "class": "input-large",
+                    'style': 'width: 50% !important; resize: vertical !important;'
+                }),  
+            }
 
-
-   def clean_name(self):
+    def clean_name(self):
        """Метод для проверки длины поля"""
        snippet_name = self.cleaned_data.get('name')
        if snippet_name is  not None and len(snippet_name) >3:
--- MainApp/views.py
+++ MainApp/views.py
@@ -11,13 +11,24 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    form = SnippetForm()
-    context = {
-        'pagename': 'Добавление нового сниппета',
-        'form' : form
-        }
+    # Создаем пустую форму при запросе GET
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {
+            'pagename': 'Добавление нового сниппета',
+            'form' : form
+            }
     
-    return render(request, 'pages/add_snippet.html', context)
+        return render(request, 'pages/add_snippet.html', context)
+    
+    # Получаем данные из формы и на их основе создаем новый сниппет сохраняя его в бд
+
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets-list") # переход на страницу списка snippets-list
+        return render(request, 'pages/add_snippet.html', context= {'form': form})    
 
 
 def snippets_page(request):
@@ -43,12 +54,5 @@ def snippet_detail(request, snippet_id):
         return render(request, 'pages/snippet_detail.html', context)
     
 
-def create_snippet(request):
-    if request.method == 'POST':
-        form = SnippetForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("snippets-list") # переход на страницу списка snippets-list
-        return render(request, 'pages/add_snippet.html', context= {'form': form})    
-    return HttpResponse('OK')        
+   
 
